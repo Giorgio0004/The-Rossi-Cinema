a8a243a7e0b65bd0a530e52865bc807d6ac95722
Giorgio0004 <93153474+Giorgio0004@users.noreply.github.com>
2022-12-08 21:55:25 +0100

    Aggiunto scelta posti

--- Project/Posti/posti.js
+++ Project/Posti/posti.js
@@ -1,30 +1,50 @@
+//data sala buttone vai
+
 let User =1;
+let posti=[];
+let prezzo = 8.70;
+let prezzoTotale;
 async function CaricaPosti(){
-    var data = {
-        id : 1,
-    };
+    
     const response = await fetch('posti.php', {
         method: 'POST',
         headers:{
             "Content-Type" : "application/json"
-        },
-        body: JSON.stringify(data)
+        }
     })
     .then(res => res.json())
     .then((data) => {
         
+        // Posti non disponibili
         for(let i=0; i<data.success.length;i++){
-            if(data.success[i].ID_User !=1){
-                //console.log(data.success[i].posto);
-                document.getElementById(data.success[i].Posto).style.backgroundColor='#ba990a';
-            }
+            document.getElementById(data.success[i].Posto).style.backgroundColor='#999999';
+            document.querySelector('#'+data.success[i].Posto).disabled = true;
         }
 
     })
 }
 
 function PrenotaPosto(posto){
-    document.getElementById(posto.value).style.backgroundColor='#ba990a';
-    //document.getElementById(posto.value).disable = true;
+    //Cambia colore
+    if(document.getElementById(posto.value).style.backgroundColor=='transparent' || document.getElementById(posto.value).style.backgroundColor==''){
+        document.getElementById(posto.value).style.backgroundColor='#000000';
+        posti.push(posto.value);
+        
+        var div = document.createElement("div");
+        div.setAttribute("id", posto.value+"_");
+        div.innerHTML=posto.value;
+        document.getElementById("div-prezzo-posti").appendChild(div);
+        prezzoTotale=prezzo*posti.length;
+    }
+    else{
+        document.getElementById(posto.value).style.backgroundColor='transparent';
+        posti.pop(posti);
+        let element = document.getElementById(posto.value+'_');
+        element.remove();
+        prezzoTotale=prezzo*posti.length;
+    }
+    prezzoTotale=prezzoTotale.toFixed(2);
+    document.getElementById("prezzo").innerHTML = prezzoTotale;
 }
+
 
--- Project/Posti/posti.php
+++ Project/Posti/posti.php
@@ -4,8 +4,6 @@ require_once "../dbConfig.php";
 
 global $dbh;
     
-$data = json_decode(file_get_contents('php://input'), true);
-$user = $data['id'];
 
 $sql = "SELECT Posto,ID_User FROM posti WHERE ID_Sessione = 1";
 $query = $dbh->prepare($sql);
