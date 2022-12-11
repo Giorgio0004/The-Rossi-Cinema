c4b4ff84cfd805b362157f7d6592ccfca591509f
Giorgio0004 <93153474+Giorgio0004@users.noreply.github.com>
2022-12-11 23:05:19 +0100

    finendo film

--- Project/Film/film.js
+++ Project/Film/film.js
@@ -1,4 +1,7 @@
-let arraySessioni;
+//passare sia ID film che ID Sessione
+
+
+let arraySessioni =[];
 let indexSessioneSelezionata;
 async function CaricaFilms(){
     let data = {
@@ -20,7 +23,7 @@ async function CaricaFilms(){
 
 async function CaricaSessioni(ID,divFilm){
     let data = {
-        sql : "SELECT sessione.ID,Sala,Data,film.Titolo,film.Copertina FROM `sessione` INNER JOIN film ON ID_Film = film.ID WHERE film.ID = "+ID,
+        sql : "SELECT sessione.ID,film.ID AS Film,Sala,Giorno,Ora,film.Titolo,film.Copertina FROM `sessione` INNER JOIN film ON ID_Film = film.ID WHERE film.ID = "+ID,
     };
     const response = await fetch('film.php', {
         method: 'POST',
@@ -31,15 +34,16 @@ async function CaricaSessioni(ID,divFilm){
     })
     .then(res => res.json())
     .then((data) => {
-        arraySessioni = data.success;
+        arraySessioni.push(data.success);
+
         for(let i=0; i<data.success.length;i++){
-        
+
             let button = document.createElement("button");
             button.setAttribute("onclick", "Sessione(this)");
-            //button.setAttribute("id", "sessione"+i);
-            button.setAttribute("value", data.success[i].ID);
-            button.setAttribute("class", "div-sessione");
-            button.innerHTML = data.success[i].Data + "\n" + data.success[i].Sala;;
+            
+            button.setAttribute("value", data.success[i].ID);   
+            button.setAttribute("class", "bg-transparent hover:bg-gray-500 text-amber-400 py-2 px-4 border border-black hover:border-white rounded mt-4 ml-2");
+            button.innerHTML = "Giorno: "+data.success[i].Giorno + "\n"+"Ora: "+data.success[i].Ora+ " Sala: "+ data.success[i].Sala;
             document.getElementById(divFilm).appendChild(button);
 
         }
@@ -47,63 +51,98 @@ async function CaricaSessioni(ID,divFilm){
 }
 
 function CreaDiv(films){
-    console.log(films.length);
+    console.log(films);
     for(let i=0;i<films.length;i++){
 
-        //crea div
         let div = document.createElement("div");
-        div.setAttribute("id", films[0].Titolo);
-        div.setAttribute("class", "div-film");
-        document.getElementById("div-films").appendChild(div);
+        div.setAttribute("id", films[i].Titolo);
+        div.setAttribute("class", "mt-8 text-center");
+        document.getElementById("films").appendChild(div);
+
+        let div1= document.createElement("div");
+        div1.setAttribute("id", films[i].Titolo+"1");
+        div1.setAttribute("class", " bg-gray-800 rounded-xl p-8  hover:bg-gray-700  inline-flex");
+        document.getElementById(films[i].Titolo).appendChild(div1);
+        
+        //crea copertina
+
+        let copertina = document.createElement("img");
+        copertina.src = films[i].Copertina;
+        copertina.setAttribute("class","max-w-xs w-80");
+        document.getElementById(films[i].Titolo+"1").appendChild(copertina);
+
+        //DivInfoFilm
+        
+        let infoFilmDiv= document.createElement("div");
+        infoFilmDiv.setAttribute("id", films[i].Titolo+"2");
+        infoFilmDiv.setAttribute("class", "block ml-4 text-start max-w-lg");
+        document.getElementById(films[i].Titolo+"1").appendChild(infoFilmDiv);
 
         //crea titolo
-        let titolo = document.createElement("h4");
-        titolo.innerHTML = films[0].Titolo;
-        document.getElementById(films[0].Titolo).appendChild(titolo);
-
-        //crea copertina
-        console.log(films[0].Copertina);
-        let copertina = document.createElement("img");
-        copertina.src = "../../assets/copertine/"+films[0].Copertina;
-        document.getElementById(films[0].Titolo).appendChild(copertina);
+        let titolo = document.createElement("h2");
+        titolo.innerHTML = films[i].Titolo;
+        titolo.setAttribute("class","text-2xl text-white mt-4")
+        document.getElementById(films[i].Titolo+"2").appendChild(titolo);
 
         //crea genere
-        let genere = document.createElement("h4");
-        genere.innerHTML = films[0].Genere;
-        document.getElementById(films[0].Titolo).appendChild(genere);
+        let genere = document.createElement("p");
+        genere.innerHTML = "Genere: "+ films[i].Genere;
+        genere.setAttribute("class","text-l font-semibold text-gray-500 mt-2");
+        document.getElementById(films[i].Titolo+"2").appendChild(genere);
         
         //crea regia
-        let regia = document.createElement("h4");
-        regia.innerHTML = films[0].Regia;
-        document.getElementById(films[0].Titolo).appendChild(regia);
+        let regia = document.createElement("p");
+        regia.innerHTML = "Regia: "+ films[i].Regia;
+        regia.setAttribute("class","text-l font-semibold text-gray-500 mt-2");
+        document.getElementById(films[i].Titolo+"2").appendChild(regia);
         
         //crea cast
-        let cast = document.createElement("h4");
-        cast.innerHTML = films[0].Cast;
-        document.getElementById(films[0].Titolo).appendChild(cast);
+        let cast = document.createElement("p");
+        cast.innerHTML = "Cast: "+films[i].Cast;
+        cast.setAttribute("class","text-l font-semibold text-gray-500 mt-2");
+        document.getElementById(films[i].Titolo+"2").appendChild(cast);
 
         //crea durata
-        let durata = document.createElement("h4");
-        durata.innerHTML = films[0].Durata;
-        document.getElementById(films[0].Titolo).appendChild(durata);
+        let durata = document.createElement("p");
+        durata.innerHTML = "Durata: "+ films[i].Durata;
+        durata.setAttribute("class","text-l font-semibold text-gray-500 mt-2");
+        document.getElementById(films[i].Titolo+"2").appendChild(durata);
 
-        CaricaSessioni(films[i].ID,films[0].Titolo);
-    }    
+        //crea div Sessioni
+        let divSessione = document.createElement("div");
+        divSessione.setAttribute("id", films[i].Titolo+"2"+" Sessione");
+        //div.setAttribute("class", "mt-8 text-center");
+        document.getElementById(films[i].Titolo+"2").appendChild(divSessione);
+        
+        CaricaSessioni(films[i].ID,films[i].Titolo+"2 Sessione");
+    }   
+     
 }
 
 
 async function Sessione(sessione){
     //passa sessione sui posti
     let FilmScelto = {Titolo:"", Copertina:""};
-    for(let i=0; i<arraySessioni.length;i++){
-        if(arraySessioni[i].ID == sessione.value){
-            FilmScelto.Titolo = arraySessioni[i].Titolo;
-            FilmScelto.Copertina = arraySessioni[i].Copertina;
-            indexSessioneSelezionata = i;
+    
+
+
+    console.log(arraySessioni[sessione.value-1]);
+
+    /*for(let i=0; i<arraySessioni.length;i++){
+        for(let y=0;y<arraySessioni[0].length;y++){
+            console.log(arraySessioni[i].ID);
+            if(arraySessioni[i].ID[y] == sessione.value){
+                console.log(arraySessioni[i].Titolo);
+                FilmScelto.Titolo = arraySessioni[i][y].Titolo;
+                FilmScelto.Copertina = arraySessioni[i][y].Copertina;
+                indexSessioneSelezionata = i;
+            }
         }
         
+        
     }
-    //console.log(FilmScelto);
+    
+    
     
     //console.log(arraySessioni[indexSessioneSelezionata]);
     let data = {
@@ -125,6 +164,5 @@ async function Sessione(sessione){
             window.location.href = "../Posti/Posti.html";
         }
         //else
-    })
+    })*/
 }
-
